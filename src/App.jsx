7dc00e2eed29cefd0157d87d525406917af2b2f7
Alejandro vargas cardoso <yepes03vargas@gmail.com>
2025-04-22 11:28:51 -0500

    Agregar logo y cambiar titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/sena.png'
 import './App.css'
 import Saludo from './components/Saludo'
 
@@ -13,14 +12,13 @@ function App() {
     <>
     
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="
+" target="_blank">
+          <img src={viteLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        
       </div>
-      <h1>Vite + React</h1>
+      <h1>Mi Primera App</h1>
       <p> <code>Contador</code> </p>{count}
 
       <div>
